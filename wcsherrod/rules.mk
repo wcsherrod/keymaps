f7e9b4362b85580ca8d2527c7d48210f9e506d51
WC Sherrod <wcsherrod@gmail.com>
2021-01-22 00:36:25 -0600

    First commit

---
+++ wcsherrod/rules.mk
@@ -0,0 +1,6 @@
+OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+LEADER_ENABLE = yes        # Enable the Leader Key feature
+MOUSEKEY_ENABLE = yes	   # Enable MouseKeys
+TAP_DANCE_ENABLE = yes	   # Enable Tap Dance
