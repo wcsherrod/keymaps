bb8ffc131fb96e489fee1cd7b4ad9ae7f20d3476
WC Sherrod <wcsherrod@gmail.com>
2021-02-04 01:16:20 -0600

    tapping term for quad, gaming layer

--- wcsherrod/keymap.c
+++ wcsherrod/keymap.c
@@ -129,6 +129,8 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
                 return TAPPING_TERM + 50;
             case LT(_LOWER, KC_BSPC):
                 return TAPPING_TERM + 50;
+            case TD(QUAD):
+                return TAPPING_TERM + 100;
             default:
                 return TAPPING_TERM;
     }   
@@ -221,7 +223,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_GAMING] = LAYOUT(
       KC_DOT,  KC_Q,   KC_W,     KC_E,   KC_R,     KC_T,                                              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    TO(_QWERTY),
       KC_TAB,  KC_A,   KC_S,     KC_D,   KC_F,     KC_G,                                              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,    KC_QUOT,
-      KC_LSFT, KC_Z,   KC_X,     KC_C,   KC_V,     KC_B,     KC_DEL, KC_LALT,       KC_LGUI, L_BACKW, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
+      KC_LSFT, KC_Z,   KC_X,     KC_C,   KC_V,     KC_B,     KC_DEL, KC_LALT,       KC_LGUI, KC_F5, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
                                  KC_ESC,   KC_M,   KC_SLSH,  KC_SPC, KC_QUOT,        KC_ENT, LT(_RAISE, KC_BSPC), L_BACKW, KC_RGUI, TD(QUAD)
     ),
 
