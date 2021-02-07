2dcb1e4b701c0e47f26421946f5d824d570ef2d4
WC Sherrod <wcsherrod@gmail.com>
2021-02-07 13:34:11 -0600

    swap shift and ctl on home row mods

--- wcsherrod/keymap.c
+++ wcsherrod/keymap.c
@@ -17,12 +17,12 @@
 // Left-hand home row mods
 #define HOME_A LT(_LOWER, KC_A)
 #define HOME_S LALT_T(KC_S)
-#define HOME_D LCTL_T(KC_D)
-#define HOME_F LSFT_T(KC_F)
+#define HOME_D LSFT_T(KC_D)
+#define HOME_F LCTL_T(KC_F)
 
 // Right-hand home row mods
-#define HOME_J RSFT_T(KC_J)
-#define HOME_K RCTL_T(KC_K)
+#define HOME_J RCTL_T(KC_J)
+#define HOME_K RSFT_T(KC_K)
 #define HOME_L LALT_T(KC_L)
 #define HOME_SCLN LT(_LOWER, KC_SCLN)
 
