e9e862284f6ec597e693cf988429529fb6fd6a0f
WC Sherrod <wcsherrod@gmail.com>
2021-02-07 14:33:49 -0600

    put enter back, fixed copy paste on raise layer

--- wcsherrod/keymap.c
+++ wcsherrod/keymap.c
@@ -155,8 +155,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
       KC_ESC,       KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
       LSFT_T(KC_TAB),  HOME_A, HOME_S, HOME_D, HOME_F, KC_G,                                KC_H, HOME_J, HOME_K, HOME_L, HOME_SCLN, RSFT_T(KC_QUOT),
-      KC_LSPO, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   LT(_NUM, KC_DEL), KC_MS_BTN2, KC_CAPS, KC_LGUI, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
-              KC_RCTL, KC_LGUI, LT(_NAV, KC_ENT), LT(_RAISE, KC_SPC), KC_MS_BTN1, L_BACKW, LT(_RAISE, KC_BSPC), LGUI_T(KC_DEL), KC_RGUI, TD(QUAD)
+      KC_LSPO, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_LGUI, KC_MS_BTN2, KC_CAPS, KC_LGUI, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
+              KC_RCTL, KC_LGUI, LT(_NAV, KC_DEL), LT(_RAISE, KC_SPC), LT(_NUM, KC_ENT), L_BACKW, LT(_RAISE, KC_BSPC), LGUI_T(KC_DEL), KC_RGUI, TD(QUAD)
     ),
 
     //     [_QWERTY] = LAYOUT(
@@ -203,7 +203,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_RAISE] = LAYOUT(
       _______, KC_1, 	  KC_2,    KC_3,    KC_4,    KC_5,                                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
       _______, KC_MS_BTN4, KC_MS_WH_DOWN, KC_MS_WH_UP, KC_MS_BTN1, KC_MS_BTN2,                                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
-      KC_LSPO, _______, _______, KC_MS_BTN2, RCTL(KC_C), RCTL(KC_V), RCTL(KC_Z), RCS(KC_Z), _______, _______, _______, KC_PGDOWN, KC_PGUP, KC_DOT, KC_SLSH, KC_RSPC,
+      KC_LSPO, _______, _______, RCTL(KC_C), RCTL(KC_V), RCTL(KC_Z), RCS(KC_Z), _______, _______, _______, _______, KC_PGDOWN, KC_PGUP, KC_DOT, KC_SLSH, KC_RSPC,
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
